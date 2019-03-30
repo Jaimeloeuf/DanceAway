2c10f9363f1621830cf672c143639793a8ac5015
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-30 09:40:12 +0800

    Fixed bug in highscore module:     Removed previously deprecated and deleted JSON_String method from utils     module and changed it to use the built in res.json method from Express

--- node_service/routes/highscore.js
+++ node_service/routes/highscore.js
@@ -13,18 +13,18 @@ const db = require('../db');
 // API to get the highscore of the user with "userID"
 router.get('/:userID', (req, res) => {
 	// Get highscore with userID and send it back as a JSON string
-	res.end(JSON_string({
+	res.json({
 		res: db.get_highscore(req.params.userID)
-	}));
+	});
 });
 
 // API to set a highscore for user with "userID"
 // Supposed to post the highscore using the request body, but much easier to implement into the URL for now
 router.post('/:userID/:score', (req, res) => {
 	// Update highscore and respond back with a boolean to indicate operation success
-	res.end(JSON_string({
+	res.json({
 		res: db.set_highscore(req.params.userID, req.params.score)
-	}));
+	});
 });
 
 module.exports = router;
