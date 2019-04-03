6e4333b46cf9ce4c672417be768587f9d514cc82
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-04-03 21:57:36 +0800

    Sprite factory function updates:     Working on changing the way Sprite objects work in the static files to     embed the reference of the img at object creation instead of displaying     it with a new method arguement every single time.
        This is experimental and just changing to see how well will this affect
        the code in terms of speed and ease of use.

--- node_service/static/Sprite.js
+++ node_service/static/Sprite.js
@@ -1,16 +1,16 @@
-// Constructor function to create Sprite objects.
-function Sprite() {
-	this.x = random(width);
-	this.y = 0;
-	this.gravity = 0.05;
-	this.velocity = 0;
+// Factory function to create Sprite objects.
+function Sprite(pic) {
+    this.x = random(width);
+    this.y = 0;
+    this.gravity = 0.05;
+    this.velocity = 0;
+    this.pic = pic;
 
-	this.show = function (pic) {
-		image(pic, this.x, this.y);
-	}
-	
-	this.update = function () {
-		this.velocity += this.gravity;
-		this.y += this.velocity;
-	}
+    this.show = (pic) => image(pic, this.x, this.y);
+    // this.show = () => image(this.pic, this.x, this.y);
+
+    this.update = function () {
+        this.velocity += this.gravity;
+        this.y += this.velocity;
+    }
 }
