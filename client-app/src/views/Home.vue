259b0d36fb1b012ab2b69576a69cd8c643892930
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-06-16 18:29:29 +0800

    Added p5 to the Client App: 1.  Added p5.js to the Vue client app using the dependency vue-p5.     It allows the creation of p5 instances in a vue component.
    2.  Created a test component for the newly installed p5 dependency in the
        Home view component.

--- client-app/src/views/Home.vue
+++ client-app/src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="home">
-    
+    <p5 v-on="this"></p5>
   </div>
 </template>
 
 <script>
+import p5 from "vue-p5";
+
 export default {
-  name: 'home'
-}
+  name: "home",
+  components: {
+    p5
+  },
+  methods: {
+    setup(sketch) {
+      sketch.background("green");
+      sketch.text("Hello p5!", 20, 20);
+    }
+  }
+};
 </script>
